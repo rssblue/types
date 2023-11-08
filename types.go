6d7d671f0549da1426b70a4f05d084650111549f
Dovydas <dovydas@yoshke.org>
2023-11-08 10:09:41 +0000

    allow to have remote items at the channel level

--- types.go
+++ types.go
@@ -51,34 +51,35 @@ func (isPresent *NSBool) MarshalXMLAttr(name xml.Name) (xml.Attr, error) {
 
 // Channel represents the podcast's feed.
 type Channel struct {
-	XMLName          xml.Name     `xml:"channel"`
-	Copyright        *string      `xml:"copyright"`
-	Description      *Description `xml:"description"`
-	Generator        *string      `xml:"generator"`
-	Language         *string      `xml:"language"`
-	LastBuildDate    *Date        `xml:"lastBuildDate"`
-	Link             *string      `xml:"link"`
-	Title            *string      `xml:"title"`
-	AtomLink         *AtomLink    `xml:"atom:link"`
-	ContentEncoded   *ContentEncoded
-	ITunesAuthor     *string `xml:"itunes:author"`
-	ITunesCategories []ITunesCategory
-	ITunesExplicit   *bool `xml:"itunes:explicit"`
-	ITunesImage      *ITunesImage
-	ITunesNewFeedURL *string `xml:"itunes:new-feed-url"`
-	ITunesOwner      *ITunesOwner
-	ITunesType       *string `xml:"itunes:type"`
-	PodcastFundings  []PodcastFunding
-	PodcastGUID      *PodcastGUID `xml:"podcast:guid"`
-	PodcastLocation  *PodcastLocation
-	PodcastLocked    *PodcastLocked
-	PodcastMedium    *PodcastMedium `xml:"podcast:medium"`
-	PodcastPersons   []PodcastPerson
-	PodcastPodping   *PodcastPodping
-	PodcastTXTs      []PodcastTXT
-	PodcastTrailers  []PodcastTrailer
-	PodcastValue     *PodcastValue
-	Items            []Item
+	XMLName            xml.Name     `xml:"channel"`
+	Copyright          *string      `xml:"copyright"`
+	Description        *Description `xml:"description"`
+	Generator          *string      `xml:"generator"`
+	Language           *string      `xml:"language"`
+	LastBuildDate      *Date        `xml:"lastBuildDate"`
+	Link               *string      `xml:"link"`
+	Title              *string      `xml:"title"`
+	AtomLink           *AtomLink    `xml:"atom:link"`
+	ContentEncoded     *ContentEncoded
+	ITunesAuthor       *string `xml:"itunes:author"`
+	ITunesCategories   []ITunesCategory
+	ITunesExplicit     *bool `xml:"itunes:explicit"`
+	ITunesImage        *ITunesImage
+	ITunesNewFeedURL   *string `xml:"itunes:new-feed-url"`
+	ITunesOwner        *ITunesOwner
+	ITunesType         *string `xml:"itunes:type"`
+	PodcastFundings    []PodcastFunding
+	PodcastGUID        *PodcastGUID `xml:"podcast:guid"`
+	PodcastLocation    *PodcastLocation
+	PodcastLocked      *PodcastLocked
+	PodcastMedium      *PodcastMedium `xml:"podcast:medium"`
+	PodcastPersons     []PodcastPerson
+	PodcastPodping     *PodcastPodping
+	PodcastRemoteItems []PodcastRemoteItem
+	PodcastTXTs        []PodcastTXT
+	PodcastTrailers    []PodcastTrailer
+	PodcastValue       *PodcastValue
+	Items              []Item
 }
 
 // Item represents episode of a podcast.
